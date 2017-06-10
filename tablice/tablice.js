5d1fcb28be9809c6dadbd8693264f1cbcf1557d2
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 15:52:15 +0200

    shift

--- tablice/tablice.js
+++ tablice/tablice.js
@@ -1,3 +1,28 @@
 'use strict';
 
 var imiona = ['Monika', 'Krystian', 'Łukasz'];
+
+console.log(imiona);
+
+imiona[3] = 'Tania';
+imiona[2] = 'Ela';
+
+console.log(imiona);
+
+//push dodaje na samym koncu element
+
+imiona.push('Geralt');
+
+console.log(imiona);
+
+imiona.pop(); //usuwa ostatni element
+
+console.log(imiona);
+
+console.log(imiona.unshift('Robert')); //unshift dodaje na poczatek roberta
+
+console.log(imiona);
+
+console.log(imiona.shift()); //wyswietla pierwszy element i jednoczesnie usuwa
+console.log(imiona);
+
