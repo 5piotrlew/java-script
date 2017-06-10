b5d84797203f5b88abaa80847127b668f50d7cee
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 11:55:28 +0200

    update

---
+++ zmienne/obiektMath/obiektMath.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var r = 10;
+
+//Pole kola = PI *r2
+
+console.log( Math.PI * Math.pow(r, 2) );
+console.log( Math.E );
+console.log( Math.cos(0) );
--- zmienne/obiektZmienne/obiektZmienne.js
+++ zmienne/obiektZmienne/obiektZmienne.js
@@ -1,2 +1,13 @@
 'use strict';
 
+//Obiekty jako zmienne
+
+var student = {
+    imie: "Kaja",
+    wzrost: 152,
+    wyswietlDane: function() { console.log(this.imie) }
+}
+
+student.wyswietlDane();
+
+console.log(student);
