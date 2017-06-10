7710c108e9b51c95f35c556164ed86453e8a4657
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 13:33:24 +0200

    do while

--- petle/petle.js
+++ petle/petle.js
@@ -1,2 +1,16 @@
-'use strict';
+//'use strict';
+//
+////petla for 
+//
+//for (var i = 4; i >= 0; i--) {
+    'use strict';
+//    console.log(i);
+//}
+//
+////while
 
+var it = 0;
+while ( it < 5 ) {
+    console.log(it);
+    it++;
+}
