c55b512d2ea02f09581b68340536cff4ffd3f0ca
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 12:20:26 +0200

    warunki

--- warunkowe/warunkowe.js
+++ warunkowe/warunkowe.js
@@ -1,2 +1,16 @@
 'use strict';
 
+var wzrostMateusz = 190;
+var wzrostOlgi = 200;
+
+if (wzrostOlgi < wzrostMateusz) {
+    console.log("Olga jest niższa");
+}
+else if (wzrostOlgi == wzrostMateusz) {
+    console.log("Olga jest równa z Mateuszem");
+}
+
+else {
+    console.log("Olga jest wyższa");
+}
+
