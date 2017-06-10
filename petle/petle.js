d866cf64fc3e8d79e3f7dbb395e1f756479093f2
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 13:44:03 +0200

    break

--- petle/petle.js
+++ petle/petle.js
@@ -9,8 +9,23 @@
 //
 ////while
 
-var it = 0;
-while ( it < 5 ) {
-    console.log(it);
-    it++;
+//var it = 0;
+//while ( it < 5 ) {
+//    console.log(it);
+//    it++;
+//}
+
+//var iter = 20;
+//do {
+//    console.log(iter);
+//    iter++;
+//} while (iter < 10 )
+
+var a = 0;
+while ( a < 10 ) {
+    console.log(++a);
+    
+    if ( a == 5 ) {
+        break;
+    }
 }
