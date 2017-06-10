4b24c1f05e701a29afd77c54885579bfedbd296a
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 14:45:53 +0200

    funkcje

---
+++ funkcje/funkcje.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function multiply(parametr1, parametr2, parametr3) {
+    
+var result;
+    
+    result = parametr1 * parametr2 * parametr3;
+    
+    return result;
+
+
+
+}
+
+var wynik = multiply(3, 9, 10);
+
+wynik = multiply(35, 2, 10);
+
+console.log("Wynik: " + wynik);
+
