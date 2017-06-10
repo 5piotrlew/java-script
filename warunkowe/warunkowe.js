ee6c6ec5437706aff5eaa812c9cf3a4606dcc872
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-10 12:28:00 +0200

    switch

--- warunkowe/warunkowe.js
+++ warunkowe/warunkowe.js
@@ -5,12 +5,26 @@ var wzrostOlgi = 200;
 
 if (wzrostOlgi < wzrostMateusz) {
     console.log("Olga jest niższa");
-}
-else if (wzrostOlgi == wzrostMateusz) {
+} else if (wzrostOlgi == wzrostMateusz) {
     console.log("Olga jest równa z Mateuszem");
-}
-
-else {
+} else {
     console.log("Olga jest wyższa");
 }
 
+//warunek switch
+
+var kolor = 'zielony';
+
+switch (kolor) {
+    case 'czerwony':
+    console.log('Kolor czerwony');
+    break;
+    case 'zielony':
+    console.log('Kolor zielony');
+    break;
+    case 'niebieski':
+    console.log('Kolor niebieski');
+    break;
+    default:
+    console.log('Inny kolor');
+}
